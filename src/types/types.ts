9cbea53a5d1bed984d786318da44c184fdb76b95
duduutech <limaedu1107@gmail.com>
2026-08-31 10:04:17 -0300

    Estrutura de pastas types

---
+++ src/types/types.ts
@@ -0,0 +1,25 @@
+export type IntegranteType = {
+  nome: string;
+  rm: string;
+  turma: string;
+  foto: string;
+  linkedin: string;
+  github: string;
+};
+
+export type FormContatoInputs = {
+  nome: string;
+  email: string;
+  mensagem: string;
+};
+
+export type FaqItemType = {
+  pergunta: string;
+  resposta: string;
+};
+
+export type PilarSolucaoType = {
+  id: string;
+  titulo: string;
+  descricao: string;
+};
