386d2c37f88abf01d33be4a5ac19d607da4664b5
duduutech <limaedu1107@gmail.com>
2026-09-01 09:23:34 -0300

    useState e toggleFaq secao de perguntas

--- src/Pages/Faq.tsx
+++ src/Pages/Faq.tsx
@@ -1,4 +1,8 @@
 import type { FaqItemType } from '../types/types';
+import { useState } from 'react';
+
+
+
 
 const listaFaq: FaqItemType[] = [
   {
@@ -22,3 +26,13 @@ const listaFaq: FaqItemType[] = [
       'Nosso sistema terá um algoritmo inteligente de pontuação que vai além de simplesmente atribuir pontos fixos por cada ação. O sistema considera múltiplos fatores antes de calcular a pontuação final do usuário. Sendo eles o tipo da ação sustentável realizada, a frequência com que o usuário realiza a ação, a consistência ao longo do tempo e o impacto sustentável gerado pela ação que o usuário realizou. Podendo ter métricas de 7, 15 e 30 dias, conforme é a atividade do usuário dentro da plataforma, pensando em seus objetivos e motivações para continuar com a atividade proposta. O usuário irá informar quantos dias de atividade quer fazer dentro de uma das métricas, assim, mesmo atingindo a meta, terá oportunidade de ultrapassar e ganhar mais pontos, por isso separamos as pontuações em 7, 15 e 30 dias. Se um usuário escolher o período de 7 dias, e acabar realizando mais atividades, passando a meta por mais 2 dias, totalizando 9 dias de atividade, o usuário irá ganhar 7 pontos pelos 7 dias mais 4 pontos pelos 2 dias que foram ultrapassado, totalizando 11 pontos. Com isso, trás a importância do sistema bonificar por atividades em vez de travar apenas na meta escolhida, trazendo efeito em sua motivação dentro da plataforma.',
   },
 ];
+
+export default function Faq() {
+  const [abertoIndex, setAbertoIndex] = useState<number | null>(null);
+
+  function toggleFaq(index: number) {
+    setAbertoIndex(abertoIndex === index ? null : index);
+  }
+
+  return null;
+}
